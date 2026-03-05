b6c94b430efc9954cbb6869ba3d6cfdca583822d
thebcrayon <106349257+thebcrayon@users.noreply.github.com>
2026-03-04 21:50:41 -0600

    Adding more sandbox exercises.

---
+++ age_calculator.rb
@@ -0,0 +1,6 @@
+puts "What is your name?"
+name = gets.chomp
+puts "When is your birth year?"
+birth_year = gets.to_i
+
+puts "Hello, #{name}. You are #{2026 - birth_year} years old!"
---
+++ name.rb
@@ -0,0 +1,5 @@
+puts "What is your first name?"
+f_name = gets.chomp
+puts "What is your last name?"
+l_name = gets.chomp
+puts "Hello, #{f_name + l_name}"
